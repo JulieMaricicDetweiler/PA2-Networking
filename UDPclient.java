a687a5fe181ac51c55f7843d3853035aa004afda
Julie Maricic-Detweiler <j.maricicdetweil@ufl.edu>
2024-03-27 15:31:34 -0400

    added folders for received images and fixed UDP to not repeat images

--- UDPclient.java
+++ UDPclient.java
@@ -1,10 +1,12 @@
 import java.io.*;
 import java.net.*;
 import java.util.Random;
+import java.util.HashSet;
 
 public class UDPclient {
     public static void main(String[] args) {
         DatagramSocket socket = null;
+        int NUM_IMAGES = 3;
         try {
             socket = new DatagramSocket();
             InetAddress serverAddress = InetAddress.getByName("localhost"); // Change to your server's IP address if not running locally
@@ -12,39 +14,44 @@ public class UDPclient {
             byte[] receiveData = new byte[65507]; // Maximum UDP packet size
 
             Random rand = new Random();
+            HashSet<Integer> sent = new HashSet<>();
 
-            // Directly request an image without user input for jokes
-            for(int i = 1; i <= 3; i++) { // Assuming you want to fetch all images in sequence or random
-                int imageNumber = rand.nextInt(3) + 1; // Randomly choose an image number
-                String request = "Image " + imageNumber; // Request a specific image
-                sendData = request.getBytes();
+            //generate requests randomly
+            while(sent.size() < NUM_IMAGES) {
+                int imageNumber = rand.nextInt(NUM_IMAGES) + 1; //random image number generation
 
-                // Send request
-                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, 1234);
-                socket.send(sendPacket);
+                if(!sent.contains(imageNumber)) { //make sure image number is not already recieved
+                    String request = "Image " + imageNumber; //request string
+                    sent.add(imageNumber);
+                    sendData = request.getBytes();
 
-                // Prepare to receive the image
-                FileOutputStream fos = new FileOutputStream("received_image" + imageNumber + ".jpg");
-                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                    // Send request
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, 1234);
+                    socket.send(sendPacket);
 
-                // Assume we're receiving a single image in multiple packets
-                boolean receiving = true;
-                while (receiving) {
-                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                    socket.receive(receivePacket); // Receive a packet
+                    // Prepare to receive the image
+                    FileOutputStream fos = new FileOutputStream("received_images_UDP/received_image" + imageNumber + ".jpg");
+                    BufferedOutputStream bos = new BufferedOutputStream(fos);
 
-                    int length = receivePacket.getLength();
-                    if (length == 0) { // Assuming the server sends a zero-length packet to indicate the end of the image transmission
-                        receiving = false;
-                    } else {
-                        // Write received data to file
-                        bos.write(receivePacket.getData(), 0, length);
+                    // Assume we're receiving a single image in multiple packets
+                    boolean receiving = true;
+                    while (receiving) {
+                        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                        socket.receive(receivePacket); // Receive a packet
+
+                        int length = receivePacket.getLength();
+                        if (length == 0) { // Assuming the server sends a zero-length packet to indicate the end of the image transmission
+                            receiving = false;
+                        } else {
+                            // Write received data to file
+                            bos.write(receivePacket.getData(), 0, length);
+                        }
                     }
-                }
 
-                bos.flush();
-                bos.close();
-                System.out.println("Image " + imageNumber + " received and saved.");
+                    bos.flush();
+                    bos.close();
+                    System.out.println("Image " + imageNumber + " received and saved.");
+                }
             }
 
         } catch (IOException e) {
